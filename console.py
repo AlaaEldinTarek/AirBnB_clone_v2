27d5ba65027a7aa7026418e9a74a2ba55a2ce702
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2023-09-22 22:36:04 +0300

    done

--- console.py
+++ console.py
@@ -2,7 +2,6 @@
 """Defines the HBNB console."""
 import cmd
 from shlex import split
-from models import storage
 from datetime import datetime
 from models.base_model import BaseModel
 from models.user import User

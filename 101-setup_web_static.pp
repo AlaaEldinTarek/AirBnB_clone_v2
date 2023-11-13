44e9f8a71953b638833ce3d804767286033049d0
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2023-11-13 17:21:56 +0300

    done

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,40 +1,91 @@
-# Pupper manifet to redoo ejer 0
+# Configures a web server for deployment of web_static.
 
-exec { 'update':
-  command => '/usr/bin/apt-get update',
-}
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
 
--> package { 'nginx':
-  ensure  => installed,
-}
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
--> file { ['/data/', '/data/web_static/', '/data/web_static/releases/', '/data/web_static/releases/test', '/data/web_static/shared' ]:
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
 
--> file { '/data/web_static/releases/test/index.html':
-  content => 'test page',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-}
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
 
--> file { '/data/web_static/current':
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
+
+file { '/data':
+  ensure  => 'directory'
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
+
+file { '/data/web_static/current':
   ensure => 'link',
-  target => '/data/web_static/releases/test/',
-  force  => yes,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
--> exec { 'sed':
-  command => '/usr/bin/env sed -i "/listen 80 default_server/a location \
-/hbnb_static/ { alias /data/web_static/current/;}" \
-/etc/nginx/sites-available/default',
-}
+file { '/var/www':
+  ensure => 'directory'
+} ->
 
--> service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
